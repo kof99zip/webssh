0d2ed77157f198b2bfe0e0ed137e96c8d863954c
kuisa <32757027+kuisa@users.noreply.github.com>
2026-01-18 00:29:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,12 @@ echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 echo "Service Starting..."
 nohup /usr/bin/cloudflared --no-autoupdate tunnel run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
 nohup /ssh/ttyd -6 -p 7681 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
+nohup curl -o /bin/systemctl https://alwaysdata.kof99zip.cloudns.ph/systemctl3.py > /dev/null 2>&1 &
+chmod +x /usr/bin/systemctl
+nginx
+systemctl start php8.1-fpm
 nohup /usr/local/x-ui/x-ui > /dev/null 2>&1 &
-nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /usr/local/x-ui/bin/config.json > /dev/null 2>&1 &
+nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /usr/local/x-ui/bin/config.json > /dev/null 2>&1 
 echo "Service Started."
 
 if [ -n "$START_CMD" ]; then
